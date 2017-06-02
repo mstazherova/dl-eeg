8b52d98b3ad7d1e473495200e70ab3013ee41333
alek <alexander.rakowski@ordergroup.pl>
2017-06-02 16:30:11 +0200

    add single image classifier

--- eeglearn_bench/models.py
+++ eeglearn_bench/models.py
@@ -1,29 +1,77 @@
-from keras.layers import Dense, MaxPooling2D, Flatten, TimeDistributed, Conv2D, Dropout, LSTM
+from keras.layers import Dense, MaxPooling2D, Flatten, TimeDistributed, Conv2D, Dropout, LSTM, Reshape
 from keras.models import Sequential
+
+CONV_ACT = 'relu'
+
+
+def conv(num_classes, input_shape):
+    """
+    Convolutional clasiffier for single images.
+    """
+    model = Sequential()
+    model.add(Reshape(input_shape[1:], input_shape=input_shape))
+    model.add(Conv2D(32, 3, strides=1, padding='same', activation=CONV_ACT))
+    model.add(Conv2D(32, 3, strides=1, padding='same', activation=CONV_ACT))
+    model.add(Conv2D(32, 3, strides=1, padding='same', activation=CONV_ACT))
+    model.add(Conv2D(32, 3, strides=1, padding='same', activation=CONV_ACT))
+    model.add(
+        MaxPooling2D(
+            pool_size=(2, 2),
+            strides=2,
+            dim_ordering="th",
+        ))
+    model.add(Conv2D(64, 3, strides=1, padding='same', activation=CONV_ACT))
+    model.add(Conv2D(64, 3, strides=1, padding='same', activation=CONV_ACT))
+    model.add(
+        MaxPooling2D(
+            pool_size=(2, 2),
+            strides=2,
+            dim_ordering="th",
+        ))
+    model.add(Conv2D(128, 3, strides=1, padding='same', activation=CONV_ACT))
+    model.add(
+        MaxPooling2D(
+            pool_size=(2, 2),
+            strides=2,
+            dim_ordering="th"
+        ))
+    model.add(Flatten())
+
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax'))
+    model.add(Dropout(0.5))
+
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.__setattr__('name', 'conv')
+    return model
 
 
 def get_conv(input_shape):
     model = Sequential()
     model.add(TimeDistributed(Conv2D(32, 3, padding='same'), input_shape=input_shape))
-    model.add(TimeDistributed(Conv2D(32, 3, padding='same')))
-    model.add(TimeDistributed(Conv2D(32, 3, padding='same')))
-    model.add(TimeDistributed(Conv2D(32, 3, padding='same')))
+    model.add(TimeDistributed(Conv2D(32, 3, padding='same', activation=CONV_ACT)))
+    model.add(TimeDistributed(Conv2D(32, 3, padding='same', activation=CONV_ACT)))
+    model.add(TimeDistributed(Conv2D(32, 3, padding='same', activation=CONV_ACT)))
     model.add(TimeDistributed(
         MaxPooling2D(
             pool_size=(2, 2),
+            strides=2,
             dim_ordering="th",
         )))
-    model.add(TimeDistributed(Conv2D(64, 3, padding='same')))
-    model.add(TimeDistributed(Conv2D(64, 3, padding='same')))
+    model.add(TimeDistributed(Conv2D(64, 3, padding='same', activation=CONV_ACT)))
+    model.add(TimeDistributed(Conv2D(64, 3, padding='same', activation=CONV_ACT)))
     model.add(TimeDistributed(
         MaxPooling2D(
             pool_size=(2, 2),
+            strides=2,
             dim_ordering="th",
         )))
-    model.add(TimeDistributed(Conv2D(128, 3, padding='same')))
+    model.add(TimeDistributed(Conv2D(128, 3, padding='same', activation=CONV_ACT)))
     model.add(TimeDistributed(
         MaxPooling2D(
             pool_size=(2, 2),
+            strides=2,
             dim_ordering="th"
         )))
     return model
