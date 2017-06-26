16114bfe021c4a3c9fbaa77b5e0e475f9475a8a9
alek <alexander.rakowski@ordergroup.pl>
2017-06-26 17:36:57 +0200

    Updated eeglearn_bench - split data into train and test:

--- eeglearn_bench/data.py
+++ eeglearn_bench/data.py
@@ -4,22 +4,24 @@ import h5py
 import numpy as np
 
 
-# TODO:
-# split into train/test/val sets
 class DataWrapper:
-    def __init__(self, dataset_path):
+    def __init__(self, dataset_path, train_data_perc=0.9):
         self.dataset_path = dataset_path
 
         h5_file = h5py.File(self.dataset_path, 'r')
         self.num_classes = h5_file['labels'].attrs['num_labels']
         self.data_dim = h5_file['data'].attrs['dims']
 
-    def gen_data(self, shuffle=True, loop=True):
-        h5_file = h5py.File(self.dataset_path, 'r')
-        while True:
-            data, labels = h5_file['data'], h5_file['labels']
-            data_and_labels = [(x, y) for x, y in zip(data, labels)]
+        data, labels = h5_file['data'], h5_file['labels']
+        data_and_labels = [xy for xy in zip(data, labels)]
+        random.shuffle(data_and_labels)
+        test_idx = int(train_data_perc * len(data_and_labels))
+        self.train_data = data_and_labels[:test_idx]
+        self.test_data = data_and_labels[test_idx:]
 
+    def gen_data(self, shuffle=True, loop=True, test=False):
+        data_and_labels = self.test_data if test else self.train_data
+        while True:
             if shuffle:
                 random.shuffle(data_and_labels)
 
@@ -36,7 +38,7 @@ class DataWrapper:
 
 
 if __name__ == '__main__':
-    data_wrapper = DataWrapper(dataset_path='../data/extracted.hdf5')
-    for x, y in data_wrapper.gen_data(loop=False):
-        print(x.shape)
+    data_wrapper = DataWrapper(dataset_path='/datasets/CogReplay/dl-eeg/pgram_norm.hdf5')
+    for x, y in data_wrapper.gen_data(shuffle=False, loop=False):
+        print(x.mean())
         print(y)
