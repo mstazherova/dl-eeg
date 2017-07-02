939781eb78683683b2ebca68ba5c8fa0f69aae67
Steffen Castle <steffen.castle@gmail.com>
2017-07-02 23:30:15 +0200

    Change introspection for AE model

--- introspection_ae.py
+++ introspection_ae.py
@@ -7,8 +7,10 @@ from keras.models import load_model
 import numpy as np
 import argparse
 
+from ConvolutionalAutoEncoder import ConvolutionalAutoEncoder
+
 #from lib.encoder.autoencoder import SAMPLE_RATE
-#from lib.encoder.utils import plot_mel_spectrogram
+#from utils import plot_mel_spectrogram
 
 MODEL_FILE_NAME = 'encoder.h5'
 
@@ -19,13 +21,16 @@ if __name__ == '__main__':
     parser.add_argument('-l', '--layer_index', type=int, default=-1)
     args = parser.parse_args()
 
-    model = load_model(args.model_file_name)
+    model = ConvolutionalAutoEncoder.model() #load_model(args.model_file_name)
+    model.load_weights('cae_weights.h5')
 
     model_input = model.layers[0].input
     input_shape = model_input.get_shape()
     input_width, input_height = input_shape[1], input_shape[2]
 
     filter_indexes = args.filter_indexes
+
+    out = []
 
     for filter_index in filter_indexes:
         print('Computing introspection for filter {} of model {}'.format(filter_index, args.model_file_name))
@@ -39,15 +44,14 @@ if __name__ == '__main__':
         # this function returns the loss and grads given the input data
         iterate = K.function([model_input], [loss, grads])
 
-        input_mel_data = np.random.random((1, input_width, input_height, 1))
+        input_mel_data = np.random.random((1, input_width, input_height, 3))
 
         for i in range(20):
             loss_value, grads_value = iterate([input_mel_data])
             input_mel_data += grads_value
 
-        log_S = input_mel_data.reshape(input_width, input_height)
-        plot_mel_spectrogram(sr=SAMPLE_RATE, log_S=log_S,
-                             file_to_save='{}_layer_{}_filter_{}_introspection'.format(
-                                 args.model_file_name.split('.')[0],
-                                 args.layer_index,
-                                 filter_index))
+        out.append(input_mel_data.reshape(input_width, input_height, 3))
+
+    np.save('introspection.npy', out)
+
+
