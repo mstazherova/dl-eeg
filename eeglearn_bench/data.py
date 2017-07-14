c06f06c40deecc72f25e6cbfe2152f0bc3c00aff
alek <alexander.rakowski@ordergroup.pl>
2017-07-14 18:16:49 +0200

    kFold cross validation

--- eeglearn_bench/data.py
+++ eeglearn_bench/data.py
@@ -9,15 +9,8 @@ class DataWrapper:
         if val_data_perc + test_data_perc >= 1:
             raise ValueError('Sum of test and validation data percentage must be less than 1.')
 
-        self.dataset_path = dataset_path
-        h5_file = h5py.File(self.dataset_path, 'r')
-        self.num_classes = h5_file['labels'].attrs['num_labels']
-        self.data_dim = h5_file['data'].attrs['dims']
-
-        data, labels = h5_file['data'], h5_file['labels']
-        data_and_labels = [xy for xy in zip(data, labels)]
+        data_and_labels = self.__load_data__(dataset_path=dataset_path)
         random.shuffle(data_and_labels)
-
         val_cnt = int(val_data_perc * len(data_and_labels))
         self.val_data = data_and_labels[:val_cnt]
         if test_data_perc == 0:
@@ -28,6 +21,17 @@ class DataWrapper:
             test_idx = int(test_data_perc * len(data_and_labels)) + val_cnt
             self.test_data = data_and_labels[val_cnt:test_idx]
             self.train_data = data_and_labels[test_idx:]
+
+    def __load_data__(self, dataset_path):
+        self.dataset_path = dataset_path
+        h5_file = h5py.File(self.dataset_path, 'r')
+        self.num_classes = h5_file['labels'].attrs['num_labels']
+        self.data_dim = h5_file['data'].attrs['dims']
+
+        data, labels = h5_file['data'], h5_file['labels']
+        data_and_labels = [xy for xy in zip(data, labels)]
+
+        return data_and_labels
 
     def gen_data(self, shuffle=True, loop=True, val=False, test=False):
         if val:
@@ -53,9 +57,64 @@ class DataWrapper:
                 break
 
 
+class KFoldDataWrapper(DataWrapper):
+    def __init__(self, dataset_path, k=10):
+        self.k = k
+        data_and_labels = self.__load_data__(dataset_path=dataset_path)
+        random.shuffle(data_and_labels)
+        self.data_and_labels = data_and_labels
+
+    def load_fold(self, fold_idx):
+        if fold_idx < 0 or fold_idx >= self.k:
+            raise ValueError("'k' must be in range [0, {})".format(self.k))
+
+        val_set_size = int(len(self.data_and_labels) / self.k)
+        self.val_data = self.data_and_labels[val_set_size * fold_idx:val_set_size * (fold_idx + 1)]
+        self.train_data = []
+        if fold_idx > 0:
+            self.train_data = self.data_and_labels[:val_set_size * fold_idx]
+        if val_set_size * (fold_idx + 1) < len(self.data_and_labels):
+            self.train_data.extend(self.data_and_labels[val_set_size * (fold_idx + 1):])
+
+    def gen_data(self, shuffle=True, loop=True, val=False, test=False):
+        if test:
+            raise ValueError('{} does not have a test dataset'.format(self.__class__))
+        return super().gen_data(shuffle=shuffle, loop=loop, val=val, test=False)
+
+    def get_test_set(self):
+        raise NotImplemented()
+
+
 if __name__ == '__main__':
     dataset_path = '/datasets/CogReplay/dl-eeg/pgram_norm.hdf5'
-    #dataset_path = '../data/extracted.hdf5'
+    dataset_path = '../data/extracted.hdf5'
+
+    data_wrapper = KFoldDataWrapper(dataset_path=dataset_path)
+    data_wrapper.load_fold(0)
+    gen = data_wrapper.gen_data()
+    print(next(gen))
+    print(next(gen))
+    gen = data_wrapper.gen_data(val=True)
+    print(next(gen))
+    print(next(gen))
+    data_wrapper.load_fold(9)
+    gen = data_wrapper.gen_data()
+    print(next(gen))
+    print(next(gen))
+    gen = data_wrapper.gen_data(val=True)
+    print(next(gen))
+    print(next(gen))
+    data_wrapper.load_fold(5)
+    gen = data_wrapper.gen_data()
+    print(next(gen))
+    print(next(gen))
+    gen = data_wrapper.gen_data(val=True)
+    print(next(gen))
+    print(next(gen))
+    # these should raise an exception
+    # data_wrapper.load_fold(10)
+    # data_wrapper.load_fold(-1)
+
     data_wrapper = DataWrapper(dataset_path=dataset_path, test_data_perc=0.1)
     for x, y in data_wrapper.gen_data(shuffle=False, loop=False):
         print(x.mean())
--- eeglearn_bench/train.py
+++ eeglearn_bench/train.py
@@ -5,7 +5,7 @@ import datetime
 from keras.callbacks import ModelCheckpoint
 from keras.callbacks import TensorBoard
 from eeglearn_bench import models
-from eeglearn_bench.data import DataWrapper
+from eeglearn_bench.data import *
 
 CHECKPOINTS_DIR = './checkpoints'
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -22,45 +22,98 @@ def make_dirs(*args):
             os.makedirs(path)
 
 
-def train(train_data_path):
-    data_wrapper = DataWrapper(dataset_path=train_data_path, test_data_perc=0.1)
-    # Create the model
-    model = models.lstm(
-        num_classes=data_wrapper.num_classes,
-        input_shape=data_wrapper.data_dim,
-    )
-    print(model.summary())
+def train(args):
+    # load the data
+    print('Loading data')
+    if args.kfold:
+        data_wrapper = KFoldDataWrapper(
+            dataset_path=args.train_data,
+            k=args.folds,
+        )
+    else:
+        data_wrapper = DataWrapper(
+            dataset_path=args.train_data,
+            val_data_perc=args.val_data,
+            test_data_perc=args.test_data
+        )
 
-    checkpoints_filepath = os.path.join(CHECKPOINTS_DIR,
-                                        '{}_{}.hdf5'.format(model.name, datetime.datetime.now().strftime("%Y%m%d_%H%M")
-                                                            ))
-    tensorboard_path = './tensorboard/{}_{}'.format(model.name, datetime.datetime.now().strftime("%Y%m%d_%H%M"))
-    make_dirs(tensorboard_path)
-    callbacks = [TensorBoard(log_dir='./tensorboard', write_images=True),
-                 ModelCheckpoint(filepath=checkpoints_filepath,
-                                 verbose=1,
-                                 save_best_only=True),
-                 ]
+    num_runs = args.folds if args.kfold else 1
+    accuracies = [0 for _ in range(num_runs)]
+    for run_idx in range(num_runs):
+        print('\n####### RUN {} ##########\n'.format(run_idx))
 
-    # Train the model
-    model.fit_generator(
-        data_wrapper.gen_data(),
-        epochs=1500,
-        steps_per_epoch=64,
-        validation_data=data_wrapper.gen_data(val=True),
-        validation_steps=16,
-        callbacks=callbacks,
-    )
-    # Evaluate the model
-    scores = model.evaluate_generator(data_wrapper.gen_data(test=True), steps=1000)
-    print("Accuracy: {}%".format(scores[1] * 100))
+        # Create the model
+        model = models.lstm(
+            num_classes=data_wrapper.num_classes,
+            input_shape=data_wrapper.data_dim,
+        )
+        if run_idx < 1:
+            print(model.summary())
+
+        checkpoints_filepath = os.path.join(CHECKPOINTS_DIR,
+                                            '{}_{}{}.hdf5'.format(model.name,
+                                                                  datetime.datetime.now().strftime("%Y%m%d_%H%M"),
+                                                                  '_{}'.format(run_idx) if args.kfold else '',
+                                                                  ))
+        tensorboard_path = './tensorboard/{}_{}'.format(
+            model.name,
+            datetime.datetime.now().strftime("%Y%m%d_%H%M"),
+            '_{}'.format(run_idx) if args.kfold else '',
+        )
+        make_dirs(tensorboard_path)
+        callbacks = [
+            TensorBoard(log_dir='./tensorboard', write_images=True),
+            ModelCheckpoint(filepath=checkpoints_filepath,
+                            verbose=1,
+                            save_best_only=True),
+        ]
+
+        if args.kfold:
+            data_wrapper.load_fold(run_idx)
+
+        # Train the model
+        model.fit_generator(
+            data_wrapper.gen_data(),
+            epochs=args.epochs,
+            steps_per_epoch=args.steps_train,
+            validation_data=data_wrapper.gen_data(val=True),
+            validation_steps=args.steps_val,
+            callbacks=callbacks,
+        )
+
+        # Evaluate the model
+        acc, total = 0, 0
+        if args.kfold:
+            gen = data_wrapper.gen_data(loop=False, val=True, shuffle=False)
+        else:
+            gen = data_wrapper.gen_data(loop=False, test=True, shuffle=False)
+        # load the best model
+        try:
+            model.load_weights(checkpoints_filepath)
+            for x, y in gen:
+                y_ = model.predict(x)
+                acc += 1 if y_.argmax() == y.argmax() else 0
+                total += 1
+            accuracy = acc * 100 / total
+            accuracies[run_idx] = accuracy
+            print("Accuracy: {}%".format(accuracy))
+        except OSError:
+            print('No model file found. The model probably didnt learn anything')
+
+    if args.kfold:
+        print(accuracies)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='EEGLearn benchmarks')
     parser.add_argument('-t', '--train_data', default='../data/extracted.hdf5')
+    parser.add_argument('-e', '--epochs', default=1500)
+    parser.add_argument('--steps_train', default=16)
+    parser.add_argument('--steps_val', default=32)
+    parser.add_argument('--val_data', default=0.1)
+    parser.add_argument('--test_data', default=0.1)
+    parser.add_argument('--kfold', action='store_true')
+    parser.add_argument('--folds', default=10)
     args = parser.parse_args()
 
-    train(
-        train_data_path=args.train_data,
-    )
+    train(args)
